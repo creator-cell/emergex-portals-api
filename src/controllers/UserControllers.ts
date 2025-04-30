f5d5c1ef4a551947a59097520a7884a3eda03ade
gmahur7 <gm7017308@gmail.com>
2025-04-30 13:30:16 +0530

    add: verify token

--- src/controllers/UserControllers.ts
+++ src/controllers/UserControllers.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { ICustomRequest } from "../types/express";
 import UserModel from "../models/UserModel";
 import { GlobalAdminRoles } from "../config/global-enum";
+import jwt from "jsonwebtoken";
+import { config } from "../config";
 
 exports.getAllUsers = async (req: Request, res: Response) => {
   const customReq = req as ICustomRequest;
@@ -26,6 +28,46 @@ exports.getAllUsers = async (req: Request, res: Response) => {
     return res.status(500).json({
       success: false,
       error: "Server error in fetching users",
+    });
+  }
+};
+
+export const verifyToken = async (req: Request, res: Response) => {
+  const token = req.headers["authorization"]?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      error: "Access denied. No token provided.",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+
+    const userId = typeof decoded === "object" && "id" in decoded ? decoded.id : null;
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid token payload.",
+      });
+    }
+
+    const user = await UserModel.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        error: "User not found.",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Token verified successfully.",
+      data: user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error: "Invalid token.",
     });
   }
 };
--- src/routes/UsersRoute.ts
+++ src/routes/UsersRoute.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { authenticate } from '../middlewares/authMiddleware';
 import { authorizeRoles } from '../middlewares/roleMiddleware';
 import { GlobalAdminRoles } from '../config/global-enum';
+import {verifyToken} from '../controllers/UserControllers'
 
 const router = express.Router();
 router.get('/super-admin', authenticate, authorizeRoles(GlobalAdminRoles.SuperAdmin), (req, res) => {
@@ -15,5 +16,7 @@ router.get('/client-admin', authenticate, authorizeRoles('client-admin', 'super-
 router.get('/viewer', authenticate, (req, res) => {
   res.status(200).json({ message: 'Welcome, Viewer!' });
 });
+
+router.route('/verify-token').get(authenticate,verifyToken)
 
 export default router
