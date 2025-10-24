21fd58eb916add264a9fb45d99106e67a7006864
Mohnish Vishwakarma <124653965+mohnish201@users.noreply.github.com>
2025-10-24 17:10:01 +0530

    Delete dist directory

--- dist/config/openAiClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const openai_1 = require("openai");
-const openai = new openai_1.OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-exports.default = openai;
