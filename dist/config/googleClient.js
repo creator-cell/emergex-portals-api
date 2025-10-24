ae41b13dfb9b295a78e6f2aff44d02531e0360fb
Mohnish Vishwakarma <124653965+mohnish201@users.noreply.github.com>
2025-10-24 17:10:23 +0530

    Delete dist directory

--- dist/config/googleClient.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import { SpeechClient } from '@google-cloud/speech';
-Object.defineProperty(exports, "__esModule", { value: true });
-// const speechClient = new SpeechClient({
-//   keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS || "aalaahah",
-// });
-// export default speechClient;
